c37f553734de604149e12949e2a603ddd21c7576
Affan <affan162ahmed@gmail.com>
2026-02-10 20:18:11 +0530

    Add files via upload

---
+++ Basic Logic/Lists.py
@@ -0,0 +1,20 @@
+my_list = ["Phone","Table","Bench","Glasses","Gooogo"]
+
+#print(my_list)
+#print(my_list[0])
+
+print(len(my_list))#len is the number of elements in a range
+#Taking user input
+#x = input("Add to list:")
+#my_list.append(x)
+#print(my_list)
+
+#Replacing an element
+#y = input("Add to list:")
+#g = int(input("Element to replace:"))
+#my_list.append(g,g)
+
+#for m in my_list: #prints all elements of the list for a random variable "m"
+    #print(m)
+for m in range(len(my_list)): 
+    print(m)
---
+++ Basic Logic/Random Integer.py
@@ -0,0 +1,9 @@
+from machine import Pin
+from time import sleep
+from random import randint
+
+x = 
+while True:
+    r = randint(1,100)
+    print(r)
+    sleep(0.05)
